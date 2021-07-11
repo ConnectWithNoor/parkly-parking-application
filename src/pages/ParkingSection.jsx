345bf0c7a8f62a70809318280f18c2668cdde567
Noor Muhammad <connectwithnoor1@gmail.com>
2021-07-11 12:55:39 +0500

    parking space page code optimized

--- src/pages/ParkingSection.jsx
+++ src/pages/ParkingSection.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { Col, Row, Button, Divider } from 'antd';
+import React, { useState, lazy, Suspense } from 'react';
+import { Col, Row, Button, Divider, Spin, Image } from 'antd';
 import { useHistory } from 'react-router-dom';
 
 import AppLayout from '../Layout/AppLayout';
@@ -9,7 +9,7 @@ import spaceTwo from '../assets/images/parking-space-2.jpg';
 import spaceThree from '../assets/images/parking-space-3.jpg';
 
 function ParkingSection() {
-  const [parkingSpace, SetParkingSpace] = useState(null);
+  const [parkingSpace, setParkingSpace] = useState(null);
   const history = useHistory();
 
   const handleNextStep = () => {
@@ -34,52 +34,56 @@ function ParkingSection() {
         <div className='mt-1rem'>
           <Row>
             <Col xs={24} sm={24} md={8} lg={8} xl={8}>
-              <div
-                className={`t-center w-100 p-1rem hover-effect ${
-                  parkingSpace === 1 && 'selected'
-                }`}
-                onClick={() => SetParkingSpace(1)}>
-                <span>Section 1</span>
-                <img
-                  src={spaceOne}
-                  alt='space-1'
-                  className='w-100 h-70 radius-1 mt-1rem'
-                />
-              </div>
+              <ImageGrid
+                ParkingSpaceId={1}
+                parkingSpace={parkingSpace}
+                setParkingSpace={setParkingSpace}
+                imgSrc={spaceOne}
+              />
             </Col>
             <Col xs={24} sm={24} md={8} lg={8} xl={8}>
-              <div
-                className={`t-center w-100 p-1rem hover-effect ${
-                  parkingSpace === 2 && 'selected'
-                }`}
-                onClick={() => SetParkingSpace(2)}>
-                <span>Section 2</span>
-
-                <img
-                  src={spaceTwo}
-                  alt='space-2'
-                  className='w-100 h-70 radius-1 mt-1rem'
-                />
-              </div>
+              <ImageGrid
+                ParkingSpaceId={2}
+                parkingSpace={parkingSpace}
+                setParkingSpace={setParkingSpace}
+                imgSrc={spaceTwo}
+              />
             </Col>
             <Col xs={24} sm={24} md={8} lg={8} xl={8}>
-              <div
-                className={`t-center w-100 p-1rem hover-effect ${
-                  parkingSpace === 3 && 'selected'
-                }`}
-                onClick={() => SetParkingSpace(3)}>
-                <span>Section 3</span>
-                <img
-                  src={spaceThree}
-                  alt='space-3'
-                  className='w-100 h-70 radius-1 mt-1rem'
-                />
-              </div>
+              <ImageGrid
+                ParkingSpaceId={3}
+                parkingSpace={parkingSpace}
+                setParkingSpace={setParkingSpace}
+                imgSrc={spaceThree}
+              />
             </Col>
           </Row>
         </div>
       </div>
     </AppLayout>
+  );
+}
+
+function ImageGrid({ setParkingSpace, ParkingSpaceId, parkingSpace, imgSrc }) {
+  return (
+    <div
+      className={`t-center w-100 p-1rem hover-effect ${
+        parkingSpace === ParkingSpaceId && 'selected'
+      }`}
+      onClick={() => setParkingSpace(ParkingSpaceId)}>
+      <span>Section {ParkingSpaceId}</span>
+      <Image
+        src={imgSrc}
+        alt={ParkingSpaceId}
+        className='w-100 h-70 radius-1 mt-1rem'
+        preview={false}
+        placeholder={
+          <div className='w-100 h-100'>
+            <Spin spinning />
+          </div>
+        }
+      />
+    </div>
   );
 }
 
